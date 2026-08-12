23af8265e9d17f7261e3d0ac3f8125cad722b8fa
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-12 15:28:14 +0900

    Load homepage signup announcement banner

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,7 @@
 import('./member-sync.js').catch((error) => console.error('[RE:LIM MEMBER SYNC]', error));
 import('./traffic.js').catch((error) => console.warn('[RE:LIM TRAFFIC LOAD]', error));
 import('./motion.js?v=20260812-ops2').catch((error) => console.warn('[RE:LIM MOTION LOAD]', error));
+import('./home-signup-banner.js?v=20260812-1').catch((error) => console.warn('[RE:LIM HOME BANNER]', error));
 
 if (document.querySelector('[data-reviews-page]')) {
   import('./collected-reviews.js?v=20260812-1').catch((error) => console.warn('[RE:LIM COLLECTED REVIEWS]', error));
