311b3d09630147c7dd276fce37e52ab8f0394ad7
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-09-11 12:45:07 +0900

    Use safer RE:LIM CMS runtime

--- assets/js/member-sync.js
+++ assets/js/member-sync.js
@@ -1,4 +1,4 @@
-import('./site-cms.js?v=20260911-cms1').catch((error) => console.warn('[RE:LIM CMS LOAD]', error));
+import('./site-cms-v2.js?v=20260911-cms2').catch((error) => console.warn('[RE:LIM CMS LOAD]', error));
 
 import { initializeApp, getApps } from 'https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js';
 import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js';
